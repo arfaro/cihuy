09d460053a2e2b2ea9da1bb50a9a230417d67985
arfaro <cikasarung@gmail.com>
2021-03-12 10:29:27 +0700

    ngedit judul

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Login extends CI_Controller {
     public function index()
     {
+        $data['title'] = "Login Coy";
         $this->load->view('login_v');
     }
 }
